a5057c7c679cdc9103d82a39fc42f4c8acbe1ee3
Robert Scott <code@humanleg.org.uk>
2020-01-18 17:14:28 +0000

    add bulk of forkserver code

---
+++ cpytraceafl/__init__.py
@@ -0,0 +1,67 @@
+import os
+import struct
+import sys
+
+import sysv_ipc
+
+from cpytraceafl import tracehook
+
+
+FORKSRV_FD = 198
+MAP_SIZE_BITS = 16
+SHM_ENV_VAR = "__AFL_SHM_ID"
+
+
+def install_trace_hook(map_start_addr, map_size_bits=None):
+    map_size_bits = map_size_bits or MAP_SIZE_BITS
+    tracehook.set_map_start(map_start_addr)
+    tracehook.set_map_size_bits(map_size_bits)
+
+    sys.settrace(tracehook.global_trace_hook)
+
+
+def attach_afl_map_shm(shm_env_var=None):
+    shm_env_var = shm_env_var or SHM_ENV_VAR
+    shm = sysv_ipc.attach(int(os.environ[shm_env_var]))
+    shm.write(b"\x01", offset=0)
+    return shm
+
+
+def cheap_excepthook(*a, **k):
+    sys.exit(99)
+
+
+def forkserver(forksrv_read_fd=None, forksrv_write_fd=None):
+    forksrv_read_fd = forksrv_read_fd or FORKSRV_FD
+    forksrv_write_fd = forksrv_write_fd or (forksrv_read_fd + 1)
+
+    forksrv_reader = open(forksrv_read_fd, "rb", buffering=0)
+    forksrv_writer = open(forksrv_write_fd, "wb", buffering=0)
+
+    # tell parent we're alive
+    forksrv_writer.write(b"\0" * 4)
+
+    while True:
+        # check parent is alive
+        forksrv_reader.read(4)
+
+        child_pid = os.fork()
+
+        if not child_pid:
+            # we are the child
+            forksrv_reader.close()
+            forksrv_writer.close()
+            return
+
+        # we are the parent. continue in loop forever.
+        forksrv_writer.write(struct.pack("I", child_pid))
+        _, child_exit_status = os.waitpid(child_pid, 0)
+        forksrv_writer.write(struct.pack("I", child_exit_status))
+
+
+def fuzz_from_here(excepthook=cheap_excepthook):
+    shm = attach_afl_map_shm()
+    forkserver()
+    install_trace_hook(shm.address)
+    if excepthook:
+        sys.excepthook = excepthook
