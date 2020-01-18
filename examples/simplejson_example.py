40d857c589c3018eb3cd6cdcf3793bee878113bb
Robert Scott <code@humanleg.org.uk>
2020-01-18 17:16:27 +0000

    add simplejson example harness

---
+++ examples/simplejson_example.py
@@ -0,0 +1,19 @@
+from cpytraceafl.rewriter import install_rewriter
+
+install_rewriter()
+
+import simplejson
+
+simplejson._toggle_speedups(False)
+simplejson.loads('{"foo": "bar", "baz": ["qux", 123, false]}')
+
+import sys
+from cpytraceafl import fuzz_from_here
+
+fuzz_from_here()
+
+with open(sys.argv[1], "rb") as f:
+    try:
+        simplejson.loads(f.read())
+    except simplejson.JSONDecodeError:
+        pass
