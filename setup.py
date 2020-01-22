e02e8ddeb105898a40620c8c5aa32241278390d8
Robert Scott <code@humanleg.org.uk>
2020-01-22 23:08:30 +0000

    setup.py: use setuptools to add basic dependency declaration

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from distutils.core import setup, Extension
+from os import path
+from setuptools import setup, Extension, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+version_namespace = {}
+with open(path.join(here, "cpytraceafl/version.py")) as f:
+    exec(f.read(), version_namespace)
 
 tracehookmodule = Extension(
     "tracehook",
@@ -6,8 +13,11 @@ tracehookmodule = Extension(
 )
 
 setup(
-    name = "cpytraceafl",
-    version = "0.1",
-    description = "Foo",
-    ext_modules = [tracehookmodule],
+    name="cpytraceafl",
+    version=version_namespace["__version__"],
+    description="CPython bytecode instrumentation and forkserver tools for fuzzing python code using AFL",
+    ext_modules=[tracehookmodule],
+    packages=find_packages(),
+    install_requires=["sysv_ipc"],
+    python_requires="==3.7",  # specifically cpython 3.7 - not tested with any other version (yet)
 )
