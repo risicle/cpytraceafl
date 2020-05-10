1e31c2068490fc3e02fdeec787763fc2a7e4ef9d
Robert Scott <code@humanleg.org.uk>
2020-05-10 15:53:12 +0100

    test_line_trace_hook: add cases testing "never zero" behaviour
    also allowing us to check existing other visit counts in the map are
    unaffected.

--- tests/test_tracehook.py
+++ tests/test_tracehook.py
@@ -9,30 +9,40 @@ import pytest
 from cpytraceafl import tracehook
 
 
-@pytest.mark.parametrize("map_size_bits,ngram_size,lineno_lasti_pairs,expected_inc_loc", (
-    (16, 0, ((123, 234), (345, 456)), 0x19a5,),
-    (16, 0, ((124, 234), (345, 456)), 0x7d19,),
-    (16, 0, ((567, 678), (123, 234), (345, 456)), 0x19a5,),
-    (16, 0, ((0, 0), (0, 0)), 0x9b6d,),
+def _get_populated_map_bytes(map_size_bits, loc_value_pairs):
+    _map = bytearray(repeat(0, 1<<map_size_bits))
+    for loc, value in loc_value_pairs:
+        _map[loc] = value
+    return bytes(_map)
+
+
+@pytest.mark.parametrize("map_size_bits,ngram_size,map_prepop,lineno_lasti_pairs,expected_nonzeros", (
+    (16, 0, (), ((123, 234), (345, 456)), ((0x19a5, 1),),),
+    (16, 0, (), ((124, 234), (345, 456)), ((0x7d19, 1),),),
+    (16, 0, (), ((567, 678), (123, 234), (345, 456)), ((0x19a5, 1),),),
+    (16, 0, (), ((0, 0), (0, 0)), ((0x9b6d, 1),),),
     # alias because of zero-multiplication avoidance, but extremely unlikely in practice
-    (16, 0, ((0, 0), (0, 0xffffffff)), 0x9b6d,),
-    (16, 0, ((0, 0), (0, 1)), 0x6492,),
+    (16, 0, (), ((0, 0), (0, 0xffffffff)), ((0x9b6d, 1),),),
+    (16, 0, (), ((0, 0), (0, 1)), ((0x6492, 1),),),
     # alias because of symmetry of operations, again extremely unlikely as lineno is usually
     # offset by a startlineno which we seed
-    (16, 0, ((0, 0), (1, 0)), 0x6492,),
-    (12, 0, ((111, 222), (333, 444)), 0x998,),
+    (16, 0, (), ((0, 0), (1, 0)), ((0x6492, 1),),),
+    (12, 0, (), ((111, 222), (333, 444)), ((0x998, 1),),),
 
     # tests with a non-zero ngram_size must make at least enough calls to flush their full
     # ngram buffer, else result will depend on pre-test state of buffer
-    (16, 2, ((567, 678), (123, 234), (345, 456)), 0x25df,),
-    (16, 2, ((789, 890), (567, 678), (123, 234), (345, 456)), 0x25df,),
-    (16, 3, ((789, 890), (567, 678), (123, 234), (345, 456)), 0x7c5b,),
-    (16, 3, ((789, 891), (567, 678), (123, 234), (345, 456)), 0x0f3f,),
-))
-def test_line_trace_hook(map_size_bits, ngram_size, lineno_lasti_pairs, expected_inc_loc):
-    expected_map = bytearray(repeat(0, 1<<map_size_bits))
-    expected_map[expected_inc_loc] = 1
+    (16, 2, (), ((567, 678), (123, 234), (345, 456)), ((0x25df, 1),),),
+    (16, 2, (), ((789, 890), (567, 678), (123, 234), (345, 456)), ((0x25df, 1),),),
+    (16, 3, (), ((789, 890), (567, 678), (123, 234), (345, 456)), ((0x7c5b, 1),),),
+    (16, 3, (), ((789, 891), (567, 678), (123, 234), (345, 456)), ((0x0f3f, 1),),),
 
+    # test never-zero overflow behaviour
+    (12, 0, ((0xa88, 0xfe),), ((321, 432), (543, 654)), ((0xa88, 0xff),),),
+    (12, 0, ((0xa88, 0xff),), ((321, 432), (543, 654)), ((0xa88, 1),),),
+
+    (16, 0, ((0x123, 0x11),), ((322, 433), (544, 655)), ((0x123, 0x11), (0x1179, 1),),),
+))
+def test_line_trace_hook(map_size_bits, ngram_size, map_prepop, lineno_lasti_pairs, expected_nonzeros):
     with mmap.mmap(-1, 1<<map_size_bits, flags=mmap.MAP_PRIVATE) as mem:
         first_byte = ctypes.c_byte.from_buffer(mem)
         try:
@@ -52,7 +62,7 @@ def test_line_trace_hook(map_size_bits, ngram_size, lineno_lasti_pairs, expected
             # we can't effectively assert the action of the prior calls as they will depend on
             # their *previous* call arguments, so clear it so we can make a clean assertion
             # following the next call
-            mem.write(bytes(repeat(0, 1<<map_size_bits)))
+            mem.write(_get_populated_map_bytes(map_size_bits, map_prepop))
             mem.seek(0)
 
             lineno, lasti = lineno_lasti_pairs[-1]
@@ -64,7 +74,7 @@ def test_line_trace_hook(map_size_bits, ngram_size, lineno_lasti_pairs, expected
             )
             tracehook.line_trace_hook(mock_frame, "line", mock.Mock())
 
-            assert mem.read() == bytes(expected_map)
+            assert mem.read() == _get_populated_map_bytes(map_size_bits, expected_nonzeros)
 
         finally:
             del first_byte
